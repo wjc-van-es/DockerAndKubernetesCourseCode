966a0dfff8f7fdc6f65ac2da96963133bdd5baf5
Willem van Es <wjc.van.es@gmail.com>
2022-02-22 23:03:10 +0100

    rolling-update small adjustements for minikube

--- samples/rolling-update/curl-loop.sh
+++ samples/rolling-update/curl-loop.sh
@@ -1,5 +1,8 @@
-for ((i=1;i<=100;i++)); 
+for ((i=1;i<=200;i++));
 do
-    curl -s "http://localhost" | grep "<title>.*</title>"
+    # with minikube we had to obtain the exact external url for the service by issueing the command:
+    # minikube service --url nginx-service
+    # curl -s "http://localhost:8080" | grep "<title>.*</title>"
+    curl -s "http://192.168.49.2:30530/" | grep "<title>.*</title>"
     sleep 2s
 done
